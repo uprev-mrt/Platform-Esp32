eee5fcad175373128d8ebcfed5c97bd47ed88236
Jason <JBerger@up-rev.com>
2021-10-12 06:29:21 -0400

    adding gatt files

---
+++ ble/mrt_gatt_abstract.c
@@ -0,0 +1,15 @@
+/**
+  *@file mrt_gatt_abstract.c
+  *@brief header for 
+  *@author dev
+  *@date 10/12/21
+  */
+
+#include "mrt_gatt_abstract.h"
+
+/**
+  *@file mrt_gatt_abstract.c
+  *@brief Abstraction of ESP32 Gatt server. Made to work with MrT GATT modules
+  *@author Jason Berger
+  *@date 10/12/21
+  */
---
+++ ble/mrt_gatt_abstract.h
@@ -0,0 +1,10 @@
+/**
+  *@file mrt_gatt_abstract.h
+  *@brief Abstraction of ESP32 Gatt server. Made to work with MrT GATT modules
+  *@author Jason Berger
+  *@date 10/12/21
+  */
+
+#pragma once
+
+
